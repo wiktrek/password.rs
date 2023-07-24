f222ca85729221b46494f4fd80c0ee0c99eb5b1c
wiktrek <wiktor.janiszewski10@gmail.com>
2023-07-25 01:36:27 +0200

    read password

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+
+#[tokio::main]
+async fn main() {
+    println!("{}", read_password());
+
+    println!("Hello, world!")
+}
+fn read_password() -> String {
+    fs::read_to_string("./passwords.json").expect("Error reading password")
 }
